fc475e305a494e5f0f3945edf8e8408396050f43
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2021-09-07 15:29:52 +0200

    Initiele versie: Open bestand, verwijder attendees, voeg category toe en opslaan in bestand.

---
+++ XeduleImportHelper/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ical.Net;
+using Ical.Net.Serialization;
+using System;
+using System.IO;
+
+namespace XeduleImportHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remove all attendees from the ics file");
+            UpdateICSFileHelper helper = new(@"d:\ICSData\2021-2022 Periode 1.ics");
+            helper.RemoveAllAttendees = true;
+            helper.AddXeduleCategory = true;
+            Console.WriteLine(helper.HandleFile());
+        }
+    }
+}
---
+++ XeduleImportHelper/UpdateICSFileHelper.cs
@@ -0,0 +1,108 @@
+﻿using Ical.Net;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeduleImportHelper
+{
+    /// <summary>
+    /// Class to import the ICS file and change the desired properties
+    /// </summary>
+    public class UpdateICSFileHelper
+    {
+        /// <summary>
+        /// Add the schedule category to each event if true
+        /// </summary>
+        public bool AddXeduleCategory { get; set; } = true;
+        /// <summary>
+        /// Remove all attendees in the file if true
+        /// </summary>
+        public bool RemoveAllAttendees { get; set; } = true;
+        /// <summary>
+        /// The sourcefile to edit
+        /// </summary>
+        private string SourceICSFile { get; set; }
+
+        /// <summary>
+        /// Contsructor to construct the helper class
+        /// </summary>
+        /// <param name="_sourceFile">The sourcefile to edit</param>
+        public UpdateICSFileHelper(string _sourceFile)
+        {
+            if (!string.IsNullOrEmpty(_sourceFile))
+            {
+                SourceICSFile = _sourceFile;
+            }
+        }
+
+        /// <summary>
+        /// Actually open the file and make the desired changes. Store the new file.
+        /// </summary>
+        /// <returns>The path to the new file</returns>
+        public string HandleFile()
+        {
+            // check file            
+            string icsFileContent;
+            if (SourceICSFile == null)
+            {
+                throw new Exception("No sourcefile defined");
+            }
+            try
+            {
+                icsFileContent = File.ReadAllText(SourceICSFile);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error reading file {SourceICSFile}", ex);
+            }
+
+            // Deserialize
+            Calendar calendar;
+            try
+            {
+                calendar = Calendar.Load(icsFileContent);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deserializing calendar", ex);
+            }
+
+            // remove all attendees
+            if (calendar != null && (RemoveAllAttendees || AddXeduleCategory))
+            {
+                foreach (var e in calendar.Events)
+                {
+                    if (RemoveAllAttendees)
+                    {
+                        e.Attendees.Clear();
+                    }
+                    if (AddXeduleCategory)
+                    {
+                        e.Categories.Add("Xedule");
+                    }
+                }
+            }
+
+            // serialize
+            string newFile;
+            try
+            {
+                newFile = $"{Path.GetDirectoryName(SourceICSFile)}\\{DateTime.Now:yyyyMMddHHmmss}_result.ics";
+                var serializer = new CalendarSerializer();
+                var serializedCalendar = serializer.SerializeToString(calendar);
+                File.WriteAllText(newFile, serializedCalendar);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error saving the new file", ex);
+            }
+
+            return newFile;
+        }
+    }
+}
