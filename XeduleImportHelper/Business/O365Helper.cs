f96123711d2fdc6315cdfee1271cfa57e2ab9b3d
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2021-09-09 09:39:09 +0200

    Start investigating Outlook integration

---
+++ XeduleImportHelper/Business/O365Helper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeduleImportHelper.Business
+{
+    /// <summary>
+    /// Helper class for communicating with Office 365
+    /// </summary>
+    class O365Helper
+    {
+    }
+}
