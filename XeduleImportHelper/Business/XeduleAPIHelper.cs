55430d8aff05d12aaf52fff841b1811a86860b7d
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2023-04-18 11:51:34 +0200

    Call API and handle file

---
+++ XeduleImportHelper/Business/XeduleAPIHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace XeduleImportHelper.Business
+{
+    public class XeduleAPIHelper
+    {
+        string fromDate = string.Empty;
+        string toDate = string.Empty;
+        int personId;
+
+        public string BearerToken  { private get; set; }
+
+        public XeduleAPIHelper(int personId)
+        {
+            if (personId == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(personId));
+            }
+            this.personId = personId;
+
+            int fromYear = DateTime.Now.Month < 8 ? DateTime.Now.Year - 1 : DateTime.Now.Year;
+            fromDate = $"{fromYear}-08-01";
+            toDate = $"{fromYear+1}-08-01";
+        }
+
+        public XeduleAPIHelper(DateTime fromDate, DateTime toDate, int personId) : this(personId)
+        {
+            this.fromDate = fromDate.ToString("yyyy-MM-dd");
+            this.toDate = toDate.ToString("yyyy-MM-dd");
+        }
+
+        public string CallAPI()
+        {
+            string url = $"https://zuyd.myx.nl/api/InternetCalendar?start={fromDate}&end={toDate}&atnId={personId}";
+          
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers["Authorization"] = $"Bearer {BearerToken}";
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            string result;
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
+            {
+                return result;
+            }
+
+            return $"{httpResponse.StatusCode}";
+        }
+
+    }
+}
--- XeduleImportHelper/Program.cs
+++ XeduleImportHelper/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XeduleImportHelper.Business;
 using XeduleImportHelper.UI;
 
 namespace XeduleImportHelper
@@ -24,10 +27,20 @@ namespace XeduleImportHelper
         [STAThread]
         static void Main()
         {
+
+            string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNyc2Etc2hhMjU2Iiwia2lkIjpudWxsLCJ0eXAiOiJKV1QifQ.eyJ1c2VyIjoiOWQwMjRiOTEtMDk4ZC00NTA1LTk4NWYtZDQyYTI0NzQ2N2UwIiwibmFtZSI6ImNpbGlzciIsImF0bklkIjoxOTI3NCwicm9sZSI6InRlYWNoZXIiLCJhdWQiOiJsaXZlLnp1eWQvQXBpIiwiaXNzIjoibGl2ZS56dXlkL0F1dGhlbnRpY2F0aW9uIiwiZXhwIjoiMTY4MTg1MjA3NCIsImlhdCI6IjE2ODE4MDg4NzQiLCJuYmYiOiIxNjgxODA4ODc0In0.D3Te9wMmO7hg5tRCa8SkoLhGrWAPG1YU-Q-d7I7QTk7k33h27PW4Y9r_V88Ze5lwcd5kPQ52pGoPioA0IlLehyP3gZzayP2YsXuZZCTXoU8-evJGO6wbWCFBfLocrQ9UJS-5DQE9pbc4Ah3m0RLdGi91vpt_8PfqGeQcL85_1RV4Z5cbtKtUNyU6w61tMkR_1EQl0yFanLAeRbI1AihZsiakHcVnaevaFOPorXuAJFKyVqbiPQ3paIorELBFWiVa-5-B6bZPDbeohiGcgJLn9i9FzbaBRPnHuI0XGAw7HK07J9LAkNZzx1DMTChkCryQaf0tbWtVGb-EUBEOQmi5Vg";
+
+            var icsResult = new XeduleAPIHelper(19274) { BearerToken = token }.CallAPI();
+            UpdateICSFileHelper helper = new UpdateICSFileHelper(icsResult, "cilissenr") { ResultPath = @"d:\CorrectieWerk" };
+            helper.RemoveAllAttendees = true;
+            helper.AddXeduleCategory = true;
+            var res = helper.HandleFile();
+
+            /*
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormMain());*/
         }
     }
 }
